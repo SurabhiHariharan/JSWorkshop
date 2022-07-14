7fa69ce319ed785904e8f1a0e2d0390522fee452
chandhana surabhi <chandhana.surabhi@toyotaconnected.co.in>
2022-07-14 14:52:41 +0530

    1

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const data = require('./data')
 
 const transformStudent = (student) =>{
     const {english, social, science} = student.marks
-    return `${student.id}, ${student.name}, English: ${english}, Social: ${social}, Science: ${science}`
+    return `${student.id}, ${student.name}, English: ${english}, Social: ${social}, Science: ${science}, Total: ${student.totalMark}`
 }
 
 const logResult = (result) => {
     console.log(result)
 }
 
+const calculateTotal = (student) =>{
+    const marks = student.marks
+    total = marks.english + marks.science + marks.social
+    student.totalMark = total
+    return student
+}
+
 const main = () => {
-       data.map(transformStudent).map(logResult)
+       data
+       .map(calculateTotal)
+       .map(transformStudent)
+       .map(logResult)
     }
 
 main()
