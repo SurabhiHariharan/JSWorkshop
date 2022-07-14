76dd8ba0b27a00a84f781b55f95b45daae382af2
chandhana surabhi <chandhana.surabhi@toyotaconnected.co.in>
2022-07-14 13:19:40 +0530

    students data

---
+++ data/index.js
@@ -0,0 +1,52 @@
+const students = [
+    {
+    id: 1,
+    name:'Subhi',
+    marks:{
+        english:100,
+        science:100,
+        social:100,
+    }
+    },
+    {
+        id:2,
+        name:'Mani',
+        marks:{
+            english:100,
+            science:100,
+            social:100,
+        }
+    },
+    {
+        id:3,
+        name:'Mano',
+        marks:{
+            english:100,
+            science:100,
+            social:100,
+        }
+    },
+    {
+        id:4,
+        name:'Hari',
+        marks:{
+            english:100,
+            science:100,
+            social:100,
+        }
+    },
+    {
+        id:5,
+        name:'Vihaan',
+        marks:{
+            english:100,
+            science:100,
+            social:100,
+        }
+    },
+
+]
+
+
+
+module.exports = students
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var data = require('./data');
-function main(){
-    console.log(data);
+const data = require('./data')
+
+const transformStudent = (student) =>{
+    const {english, social, science} = student.marks
+    return `${student.id}, ${student.name}, English: ${english}, Social: ${social}, Science: ${science}`
+}
+
+const logResult = (result) => {
+    console.log(result)
+}
+
+const main = () => {
+       data.map(transformStudent).map(logResult)
     }
 
-main();
+main()
